c8382da78fbcdae9dda4de2dfd2bba980be09885
BryanGuapulema <bryan19012003@gmail.com>
2025-08-26 00:17:51 -0500

    custom errors added

--- backend/schemas/recipeSchema.js
+++ backend/schemas/recipeSchema.js
@@ -1,11 +1,11 @@
 const z = require('zod')
 
 const recipeSchema = z.object({
-  title: z.string().min(1).max(100),
-  ingredients: z.array(z.string()).min(2),
-  instructions: z.string().min(20),
-  time: z.number().positive(),
-  difficulty: z.enum(['easy', 'medium', 'hard'])
+  title: z.string().min(1, 'Muy corto').max(100, 'Muy largo'),
+  ingredients: z.array(z.string(),"Los ingredientes deben estar en un array").min(2),"Se necesitam al menos dos ingredientes",
+  instructions: z.string().min(20,"El contenido debe ser de al menos 20 caracteres"),
+  time: z.number().positive("el tiempo debe ser positivo"),
+  difficulty: z.enum(['easy', 'medium', 'hard'],"la dificultad solo puede ser del tipo: easy, medium,hard")
 })
 
 function validateRecipe (object) {
