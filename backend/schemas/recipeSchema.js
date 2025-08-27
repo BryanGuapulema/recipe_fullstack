37c4ce8f88820589c693d7b3e98efc38507fc48b
BryanGuapulema <bryan19012003@gmail.com>
2025-08-26 23:01:20 -0500

    custom mesages added in schema

--- backend/schemas/recipeSchema.js
+++ backend/schemas/recipeSchema.js
@@ -2,10 +2,10 @@ const z = require('zod')
 
 const recipeSchema = z.object({
   title: z.string().min(1, 'Muy corto').max(100, 'Muy largo'),
-  ingredients: z.array(z.string(),"Los ingredientes deben estar en un array").min(2),"Se necesitam al menos dos ingredientes",
-  instructions: z.string().min(20,"El contenido debe ser de al menos 20 caracteres"),
-  time: z.number().positive("el tiempo debe ser positivo"),
-  difficulty: z.enum(['easy', 'medium', 'hard'],"la dificultad solo puede ser del tipo: easy, medium,hard")
+  ingredients: z.array(z.string(), 'Los ingredientes deben estar en un array').min(2, 'Se necesitam al menos dos ingredientes'),
+  instructions: z.string().min(20, 'El contenido debe ser de al menos 20 caracteres'),
+  time: z.number().positive('el tiempo debe ser positivo'),
+  difficulty: z.enum(['easy', 'medium', 'hard'], 'la dificultad solo puede ser del tipo: easy, medium,hard')
 })
 
 function validateRecipe (object) {
